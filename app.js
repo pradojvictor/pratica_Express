532dfc4b92db13d4a173022360e2cf068eea23d8
pradojvictor <joaovictorred@hotmail.com>
2022-05-11 16:41:58 -0300

    add pratica com Express,nodejs,npm feitos em aula

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");  // importa o express usando a funão require do node
+
+const app = express();   //inicializar o servido que esta contido em um objeto, usando o app para fazer alterções
+
+app.get("/", (requisisao, resposta)=>{
+    resposta.send("Olá mundo");
+})
+
+
+app.listen(3000, ()=> console.log("servido rodando na porta 3000"));  //porta do servido, usando uma callback para ter uma respota visual 
+
+
+//comando:
+// npm init -y   => usado pra criar
+// npm install express  => usado para baixar o pacote Express
+// node app.js          => iniciar o servidor
