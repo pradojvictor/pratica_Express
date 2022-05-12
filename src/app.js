727e06691bc3cc21f91ade1462c010353d3c7503
pradojvictor <joaovictorred@hotmail.com>
2022-05-11 21:48:50 -0300

    inicio de uma api, foi restruturado as pastas, add methodos get,put,delete,post

--- src/app.js
+++ src/app.js
@@ -2,12 +2,54 @@ const express = require("express");  // importa o express usando a funão requir
 
 const app = express();   //inicializar o servido que esta contido em um objeto, usando o app para fazer alterções
 
-app.get("/", (requisisao, resposta)=>{
-    resposta.send("Olá mundo");
+const port = 3000;
+
+app.use(express.json());                           // use esse metodo, vc vai receber json e usar json
+app.use(express.urlencoded({extended : true}));  
+
+app.get("/products", (req, res)=>{
+    res.json([]); 
+})
+
+app.post("/products", (req, res)=> {
+  res.json(req.body);
+})
+
+app.get("/products/:id", (req,res)=>{
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name:`produto ${id}`,
+    price: 300,
+  })
+})
+
+app.put("/products/:id", (req,res)=>{
+  const { id } = req.params;
+
+  res.json({
+    id,
+    ...(req.body || {}),
+  })
+})
+
+app.delete("/products/:id", (req,res)=>{
+  res.status(204).send("")
+})
+
+app.get("/:name?", (req, res)=>{
+    const { name } = req.params;
+
+    res.send(`hello ${name || "world"}`);
 })
 
 
-app.listen(3000, ()=> console.log("servido rodando na porta 3000"));  //porta do servido, usando uma callback para ter uma respota visual 
+app.listen(port, () => {
+  console.log(`Servidor executando na porta: ${port}`);  //porta do servido, usando uma callback para ter uma respota visual 
+});
+
+ 
 
 
 //comando:
